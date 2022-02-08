b998f3f2501a390c204d2409654b5972c9d93b89
Gerardo Bernal <gerardo.bernal@uanl.mx>
2022-02-07 18:22:32 -0600

    Se agrega script peticion basica en python

---
+++ peticion.py
@@ -0,0 +1,12 @@
+import requests
+#
+# Script para hacer requests a un sitio.
+if __name__ == '__main__':
+    url = "https://www.google.com.mx"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        content = response.content
+        file = open('google.html','wb')
+        file.write(content)
+        file.close()
