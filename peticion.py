40e46ba97d772f99e32eaa1f6dfb839afb590aed
yvinzo <88557059+yvinzo@users.noreply.github.com>
2022-02-07 18:52:01 -0600

    Complementar objetivo del script

--- peticion.py
+++ peticion.py
@@ -1,6 +1,7 @@
 import requests
 #
 # Script para hacer requests a un sitio.
+# y escribir el resultado a un archivo.
 if __name__ == '__main__':
     url = "https://www.google.com.mx"
     response = requests.get(url)
